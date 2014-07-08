4a05d0431c0f5cf494a86609351e28b38c810f2a
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2014-07-08 16:02:06 +0200

    Added export button

--- src/main/java/org/eobjects/datacleaner/extension/emailing/ui/SendEmailAnalyzerResultSwingRenderer.java
+++ src/main/java/org/eobjects/datacleaner/extension/emailing/ui/SendEmailAnalyzerResultSwingRenderer.java
@@ -3,8 +3,14 @@ package org.eobjects.datacleaner.extension.emailing.ui;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.util.Collection;
 
+import javax.swing.Icon;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
@@ -14,8 +20,13 @@ import org.eobjects.analyzer.result.renderer.SwingRenderingFormat;
 import org.eobjects.datacleaner.extension.emailing.EmailResult;
 import org.eobjects.datacleaner.extension.emailing.SendEmailAnalyzerResult;
 import org.eobjects.datacleaner.panels.DCPanel;
+import org.eobjects.datacleaner.util.FileFilters;
+import org.eobjects.datacleaner.util.IconUtils;
+import org.eobjects.datacleaner.util.ImageManager;
 import org.eobjects.datacleaner.util.WidgetUtils;
 import org.eobjects.datacleaner.widgets.Alignment;
+import org.eobjects.datacleaner.widgets.DCFileChooser;
+import org.eobjects.metamodel.util.FileHelper;
 import org.jdesktop.swingx.VerticalLayout;
 
 @RendererBean(SwingRenderingFormat.class)
@@ -35,6 +46,41 @@ public class SendEmailAnalyzerResultSwingRenderer extends AbstractRenderer<SendE
         panel.add(createMetricText("Emails failed: ", failureCount));
 
         final Collection<EmailResult> failures = analyzerResult.getFailures();
+
+        if (!failures.isEmpty()) {
+            final Icon icon = ImageManager.getInstance().getImageIcon(IconUtils.CSV_IMAGEPATH,
+                    IconUtils.ICON_SIZE_MEDIUM);
+            final JButton exportButton = new JButton("Export failure recipients", icon);
+            exportButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    final DCFileChooser fileChooser = new DCFileChooser();
+                    fileChooser.addChoosableFileFilter(FileFilters.TXT);
+                    final int result = fileChooser.showSaveDialog(panel);
+                    if (result == DCFileChooser.APPROVE_OPTION) {
+                        final File selectedFile = fileChooser.getSelectedFile();
+                        final BufferedWriter writer = FileHelper.getBufferedWriter(selectedFile);
+                        try {
+                            writer.write("recipient");
+                            for (EmailResult emailResult : failures) {
+                                writer.write('\n');
+                                writer.write(emailResult.getRecipient());
+                            }
+                        } catch (Exception ex) {
+                            WidgetUtils.showErrorMessage("Failed to export failure recipients", ex);
+                        } finally {
+                            FileHelper.safeClose(writer);
+                        }
+                    }
+                }
+            });
+
+            final DCPanel exportButtonPanel = new DCPanel();
+            exportButtonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+            exportButtonPanel.add(exportButton);
+            panel.add(exportButtonPanel);
+        }
+
         int failuresPresented = 0;
         for (EmailResult failureEmailResult : failures) {
             panel.add(createFailureText(failureEmailResult));
